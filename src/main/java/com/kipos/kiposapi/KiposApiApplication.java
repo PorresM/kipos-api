e5e09e7b21fc52cee6343be96076ff09d60cf91d
Martin Porres <mporres@steamulo.com>
2020-10-03 15:04:44 +0200

    [INIT] SpringBoot App

---
+++ src/main/java/com/kipos/kiposapi/KiposApiApplication.java
@@ -0,0 +1,13 @@
+package com.kipos.kiposapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiposApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiposApiApplication.class, args);
+	}
+
+}
